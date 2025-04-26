4c513ec184c8e122901fad8ec8015c61491c09c9
arren1088 <arren1088@gmail.com>
2025-04-26 18:17:07 +0800

    occupation commit

--- index.py
+++ index.py
@@ -10,5 +10,9 @@ def index():
 def result():
   return render_template("result.html")
 
+@app.route("/occupation")
+def occupation():
+  return render_template("occupation.html")
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080, debug=True)
